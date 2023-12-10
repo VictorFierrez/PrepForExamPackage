609e8c0fe0ea6e9f7e0d63b70f179dc7d2c7c359
VictorFierrez <s223196@dtu.dk>
2023-12-10 12:16:13 +0100

    Data into the package.

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Codon Translation Table
+#'
+#' A list of codons and the corresponding amino acid they encode for. Stop codons are represented as a "_" character.
+#'
+#'
+#' @source \url{https://r4bds.github.io/lab08.html}
+"codons"
